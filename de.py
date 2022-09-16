d4d93038fae95873e7f1ab7df36d7ef9669eb2b8
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2022-09-16 14:41:12 -0400

    Update de.py

--- de.py
+++ de.py
@@ -11,20 +11,22 @@ def sigmoid(x):
 
 
 #define the objective function:
-def obj_fun(parameters, data):
+def obj_fun(parameters, *data):
     para_np_arr = np.array(parameters) # convert the input data into np array
-    print(para_np_arr)
     para_transposed = np.transpose(para_np_arr)
-    data = np.asarray(data) 
+    data = np.asarray(data) # conver data into np array
     # Using logistic regression to re-scale data within [0,1]
     result = sigmoid(np.dot(data[:, :7], para_transposed))
     # assign new data with 1 when >= 0.5, 0 otherwise
-    result = result >= 0.5
-    y_test = data[:, 7]
-    y_pred = result 
-    auc = roc_auc_score(y_test, y_pred)
+    diff = np.subtract(data[:, 7], result)
+    diff = diff[~np.isnan(diff)]
+    diff_squ = np.square(diff)
+    diff_squ_sum = np.sum(diff_squ) 
+    # y_test = data[:, 7]
+    # y_pred = result 
+    # auc = roc_auc_score(y_test, y_pred)
 
-    return -1*auc
+    return diff_squ_sum
 
 
 #in the data, women='0', man='1',children='0', students='1', parents='2'
@@ -40,6 +42,7 @@ for row in range(len(data)):
 bounds = [(0.01, 3), (0.01, 3), (0.01, 3), (0.01, 3), (0.01, 3), (0.01, 3),
           (0.01, 3)]
 
-parameters = [2,2,2,2,2,2,2]
-# result = differential_evolution(obj_fun, bounds, args=data)
-print(obj_fun(parameters, data))
+# parameters = [2,2,2,2,2,2,2]
+result = differential_evolution(obj_fun, bounds, args=data)
+# print(obj_fun(parameters, data))
+print(result.x)
