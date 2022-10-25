67862de8c528b87780298fa0408ab3f65702003e
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2022-10-24 20:44:45 -0400

    Update de.py

--- de.py
+++ de.py
@@ -5,17 +5,16 @@ from sklearn import metrics
 import matplotlib.pyplot as plt
 
 
-
 def sigmoid(x):
     sig = 1 / (1 + np.exp(-x))
     return sig
 
 
-#define the objective function:
+# define the objective function:
 def obj_fun(parameters, *data):
-    para_np_arr = np.array(parameters) # convert the input data into np array
+    para_np_arr = np.array(parameters)  # convert the input data into np array
     para_transposed = np.transpose(para_np_arr)
-    data = np.asarray(data) # conver data into np array
+    data = np.asarray(data)  # conver data into np array
     # Using logistic regression to re-scale data within [0,1]
     result = np.dot(data[:, :7], para_transposed)
     # set binalize of Q8 based on value 2.5
@@ -23,7 +22,7 @@ def obj_fun(parameters, *data):
     # result[result >= 17.5] = 1
     # result[result < 17.5] = 0
     for i in range(result.shape[0]):
-        if (result[i]) >= 17.5:
+        if (result[i]) >= 10.5:
             result[i] = 1
         else:
             result[i] = 0
@@ -31,20 +30,21 @@ def obj_fun(parameters, *data):
     diff = np.subtract(data[:, 7], result)
     diff = diff[~np.isnan(diff)]
     diff_squ = np.square(diff)
-    diff_squ_sum = np.sum(diff_squ) 
+    diff_squ_sum = np.sum(diff_squ)
     # y_test = data[:, 7]
-    # y_pred = result 
+    # y_pred = result
     # auc = roc_auc_score(y_test, y_pred)
     # print(diff_squ_sum)
     return -1*diff_squ_sum
 
 
-#in the data, women='0', man='1',children='0', students='1', parents='2'
-input = np.genfromtxt('man_data_cleaned.csv', skip_header=1, delimiter=',')
+input = np.genfromtxt('woman_data_cleaned.csv', skip_header=1, delimiter=',')
 data = input[:, :8]
-#modify data in the output (8th) column
+Q8_THRESHOLD = 1.5
+Q17_THRESHOLD = 7 * Q8_THRESHOLD
+# modify data in the output (8th) column
 for row in range(len(data)):
-    if data[row][7] >= 2.5:
+    if data[row][7] >= 1.5:
         data[row][7] = 1
     else:
         data[row][7] = 0
@@ -52,25 +52,27 @@ for row in range(len(data)):
 bounds = [(0.01, 3), (0.01, 3), (0.01, 3), (0.01, 3), (0.01, 3), (0.01, 3),
           (0.01, 3)]
 
-# parameters = [2,2,2,2,2,2,2]
+# when all weights are equal to 1
+data = np.asarray(data)
+pre_result = data[:, :7].sum(axis=0)
+
 result = differential_evolution(obj_fun, bounds, args=data)
 # print(obj_fun(parameters, data))
-print(result.x)
+# print(result.x)
 
 # retrive the ROC plot of the DE result
 para = np.array(result.x)
 para_transposed = np.transpose(para)
-data = np.asarray(data)
-result_dot = np.dot(data[:,:7], para_transposed)
+result_dot = np.dot(data[:, :7], para_transposed)
 for i in range(result_dot.shape[0]):
-        if (result_dot[i]) >= 17.5:
-            result_dot[i] = 1
-        else:
-            result_dot[i] = 0
+    if (result_dot[i]) >= 10.5:
+        result_dot[i] = 1
+    else:
+        result_dot[i] = 0
 
-fpr, tpr, _ = metrics.roc_curve(data[:,7],  result_dot)
-auc = metrics.roc_auc_score(data[:,7], result_dot)
-plt.plot(fpr,tpr,label="AUC="+str(auc))
+fpr, tpr, _ = metrics.roc_curve(data[:, 7],  result_dot)
+auc = metrics.roc_auc_score(data[:, 7], result_dot)
+plt.plot(fpr, tpr, label="AUC="+str(auc))
 plt.ylabel('True Positive Rate')
 plt.xlabel('False Positive Rate')
 plt.legend(loc=4)
